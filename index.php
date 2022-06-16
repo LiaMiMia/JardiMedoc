012ce3b7302526907c06d3f62509845765c7fb0e
Elea MANE <mane.elea@gmail.com>
2022-06-16 10:05:01 +0200

    mise en place du footer pbm: underscore apres les icones reseaux

--- index.php
+++ index.php
@@ -8,9 +8,13 @@
     <!-- CSS only bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     
-    <!-- JavaScript Bundle with Popper -->
+    <!-- JavaScript Bundle with Popper bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-
+    
+    <!-- fontAwsome Icons -->
+    <!-- Option 1: Include in HTML -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
+    
     <!-- my CSS folders -->
     <link rel="stylesheet" href="css/nav.css">
     <link rel="stylesheet" href="css/welcome-card.css">
@@ -40,6 +44,11 @@
         require_once('pages/accueil.php');
         ?>
     </main>
+    <footer>
+        <?php
+        require_once('indispensables/footer.php');
+        ?>
+    </footer>
 
     <!-- Jquery necessaire au bont fonctionnement des éléments bootstrap.  -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
